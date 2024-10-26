e49f9767f98b9db4dc66984092a9a19603663dc3
Roberto <robertopadillapadilla9@gmail.com>
2024-10-26 13:44:25 -0500

    Creación de las carpetas categorias y tareas en vista donde se almacenarán las views del crud de cada una

---
+++ resources/views/categorias/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title','Crear categoría')
+
+@section('content')
+    <h1>Aquí se crearán las categorías</h1>
+@endsection
---
+++ resources/views/tareas/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title','Crear categoría')
+
+@section('content')
+    <h1>Aquí se crearán las tareas</h1>
+@endsection
