a066ec95e5d93cf15793a0dee8f2239fc9bb70c7
Roberto <robertopadillapadilla9@gmail.com>
2024-10-26 13:31:12 -0500

    Creación de grupos de rutas para cada controlador

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\CategoriaController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TareaController; 
+use Illuminate\Support\Facades\Blade;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', CategoriaController::class);
+
+Route::controller(CategoriaController::class)->group(function(){
+    Route::get('crear-categoria','Create');
+});
+
+
+Route::controller(TareaController::class)->group(function(){
+    Route::get('crear-curso','Create');
 });
